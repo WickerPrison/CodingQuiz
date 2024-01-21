5ebe0911238b8ac0063b51417c2ba3cea8398018
WickerPrison <cobylax17@gmail.com>
2024-01-21 16:27:38 -0700

    set up final score page

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,21 +2,47 @@ var timer = document.getElementById("timer");
 var question = document.getElementById("question");
 var answersElms = document.getElementsByClassName("answer");
 var scoreElm = document.getElementById("score");
+var greyOut = document.getElementById("grey-out");
+var quizResults = document.getElementById("quiz-results");
+var scoreDisplay = document.getElementById("final-score");
+var submitBtn = document.getElementById("submit-button");
+var closeBtn = document.getElementById("close-button");
 
 var questionObject;
 var shuffledAnswers;
 
-var timeInSeconds = 200;
+var timeInSeconds = 100;
 var score = 0;
 
 displayTime(timeInSeconds);
 
-setInterval(function startTimer() { 
+var intervalID = setInterval(function startTimer() { 
     timeInSeconds--;
     displayTime(timeInSeconds);
 }, 1000);
  
 loadQuestion(0);
+
+function displayTime(timeInSeconds){
+    if(timeInSeconds <= 0){
+        timeInSeconds = 0;
+        clearInterval(intervalID);
+        quizOver();
+    }
+    
+    var minutes = 0;
+    while(timeInSeconds > 59){
+        timeInSeconds -= 60;
+        minutes++;
+    }
+
+    var tensPlaceDigit = "";
+    if(timeInSeconds < 10){
+        tensPlaceDigit = 0;
+    }
+
+    timer.innerText = minutes + ":" + tensPlaceDigit + timeInSeconds;
+}
 
 function loadQuestion(index){
     questionObject = window.questionDictionary[index];
@@ -39,20 +65,10 @@ function answerQuestion(event){
     }
 }
 
-
-function displayTime(timeInSeconds){
-    var minutes = 0;
-    while(timeInSeconds > 59){
-        timeInSeconds -= 60;
-        minutes++;
-    }
-
-    var tensPlaceDigit = "";
-    if(timeInSeconds < 10){
-        tensPlaceDigit = 0;
-    }
-
-    timer.innerText = minutes + ":" + tensPlaceDigit + timeInSeconds;
+function quizOver(){
+    greyOut.style.backgroundColor = "rgba(0,0,0,0.5)";
+    quizResults.style.display = "block";
+    scoreDisplay.innerText = "Your Score: " + score;
 }
 
 // shuffles array using the Durstenfeld shuffle
@@ -67,6 +83,12 @@ function shuffleArray(originalArray) {
     return newArray;
 }
 
+function reloadPage(){
+    location.reload();
+}
+
 for(var i = 0; i < answersElms.length; i++){
     answersElms[i].addEventListener("click", answerQuestion);
 }
+closeBtn.addEventListener("click", reloadPage);
+submitBtn.addEventListener("click", reloadPage);
